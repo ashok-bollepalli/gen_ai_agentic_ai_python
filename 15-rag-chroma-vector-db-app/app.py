a8818d6742148e5d03dfd612d7a3f166863781da
ashok-bollepalli <161624348+ashok-bollepalli@users.noreply.github.com>
2026-09-23 21:52:44 +0530

    Update app.py

--- 15-rag-chroma-vector-db-app/app.py
+++ 15-rag-chroma-vector-db-app/app.py
@@ -3,19 +3,16 @@ from rag import ask_question
 
 st.title("Company RAG Chatbot")
 
-st.write(
-    "Ask questions about the company documents."
-)
+st.write("Ask questions about the company documents.")
 
-
-question = st.text_input(
-    "Enter your question:"
-)
+question = st.text_input("Enter your question:")
 
 if st.button("Ask question"):
     if question:
-      answer = ask_question(question)
-      st.subheader("Answer")
-      st.write(answer)
+        with st.spinner("🔍 Searching company documents..."):
+            answer = ask_question(question)
+
+        st.subheader("Answer")
+        st.write(answer)
     else:
         st.warning("Please enter a question")
